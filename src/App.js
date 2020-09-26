9e04a74cec213983d086c624db41130726ac6608
Sofia <sofia.dionisio94@gmail.com>
2020-09-26 11:20:43 +0100

    Displaying initial HTML elements and created initial styles

--- src/App.js
+++ src/App.js
@@ -2,11 +2,33 @@ import React, { useState } from "react";
 import "./app.css";
 
 function App() {
- return (
-   <div className="App">
-     App goes here
-   </div>
- )
+  return (
+    <div className="App">
+      <h1 className="title">Forgot your Password?</h1>
+      <h5 className="subtitle">
+        Please enter your email address below to receive a password reset link.
+      </h5>
+      <div className="reset-form">
+        <div>
+          <p className="email-label">Email Address</p>
+          <input
+            className="email"
+            type="email"
+            placeholder="Your email address"
+          />
+
+          <div className="buttons">
+            <button className="reset-btn">Reset Password</button>
+            <a href="#!">Go back</a>
+          </div>
+        </div>
+
+        <span className="instructions-btn">
+          Are you having trouble receiving your password reset?
+        </span>
+      </div>
+    </div>
+  );
 }
 
 export default App;
