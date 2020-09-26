2709bbd6b59d53f9c4081e10e1746432415975ed
Sofia <sofia.dionisio94@gmail.com>
2020-09-26 11:31:12 +0100

    Created initial state, used React hooks.Stored instructions display , to show or hide upon a click. Added some styles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React, { useState } from "react";
 import "./app.css";
 
 function App() {
+  const [instructionsDisplay, setInstructionsDisplay] = useState(false);
+
+  const handleInstructions = () => {
+    setInstructionsDisplay(!instructionsDisplay);
+  };
+
   return (
     <div className="App">
       <h1 className="title">Forgot your Password?</h1>
@@ -23,9 +29,41 @@ function App() {
           </div>
         </div>
 
-        <span className="instructions-btn">
+        <span className="instructions-btn" onClick={handleInstructions}>
           Are you having trouble receiving your password reset?
         </span>
+
+        <div
+          className={
+            instructionsDisplay ? "instructions-visible" : "instructions-hidden"
+          }
+        >
+          <p className="paragraphs">
+            We have had some customers who mistakenly believe they have store
+            account when in fact they are only signed up to receive our VIP Club
+            newsletter.
+          </p>
+          <p className="paragraphs">
+            If you receive our newsletter but have never made a purchase online
+            this might apply to you, the best way to check this is to do the
+            following:
+          </p>
+          <p className="bullet-point">
+            1. Go back to the <a href="#!">register page</a>.
+          </p>
+          <p className="bullet-point">
+            2. Register with your email address.
+            <br />
+            <span className="extra-info">
+              If you do have already an account the form will tell you
+            </span>
+          </p>
+          <p className="paragraphs">
+            If the form tells you you already have an account and the password
+            reset doesn't work please contact the Customer Service Team on 033
+            900 0094.
+          </p>
+        </div>
       </div>
     </div>
   );
